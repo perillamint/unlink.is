8723ae19087b27f6b12af074f959e77cf8da540d
perillamint <perillamint@gentoo.moe>
2015-11-27 02:17:58 +0900

    Metadata fix

--- unlinkis.js
+++ unlinkis.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name        unlink.is
-// @namespace   unlinkis.gentoo.moe
+// @namespace   http://gmscript.gentoo.moe
 // @include     https://twitter.com/*
 // @version     0.1
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
+// @downloadURL https://raw.githubusercontent.com/perillamint/unlink.is/master/unlinkis.js
+// @updateURL   https://raw.githubusercontent.com/perillamint/unlink.is/master/unlinkis.meta.js
 // @grant       GM_xmlhttpRequest
 // @license     GPLv3 or later
 // ==/UserScript==
