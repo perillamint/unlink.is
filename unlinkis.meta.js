83aec89a5e7426d25bf8700e7e7b20f9be97042d
Gaeulbyul <aeiou256@gmail.com>
2016-06-27 00:29:59 +0900

    Fix error messages on console

--- unlinkis.meta.js
+++ unlinkis.meta.js
@@ -2,6 +2,7 @@
 // @name        unlink.is
 // @namespace   http://gmscript.gentoo.moe
 // @include     https://twitter.com/*
+// @exclude     https://twitter.com/i/*
 // @version     0.2
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @downloadURL https://raw.githubusercontent.com/perillamint/unlink.is/master/unlinkis.user.js
